dcec67a966f4d8192f3f7ad23b04b7fef76facdf
Marco Dell'Anna <marco.dellanna@studenti.unipd.it>
2020-04-24 12:51:36 +0200

    Added missing interface to chat.html

--- js/chat.js
+++ js/chat.js
@@ -50,4 +50,55 @@ function filter_chatlist(ev, elem){
     ev.preventDefault();
 }//filter_chatlist
 
+// RETRIEVE THE POP-UP FOR LESSON PROPOSAL
+var modal = document.querySelector(".lesson_proposal");
+
+// CONTROL VARIABLE TO TOGGLE THE LESSON PROPOSAL POP-UP
+var modal_ctrl = false;
+
+function toggle_modal(ev) {
+
+    // IF THE POP-UP IS VISIBLE
+    if(modal_ctrl){
+
+        // SWITCH THE CONTROL VARIABLE OFF 
+        modal_ctrl = false;
+
+        // MAKE IT DISAPPEAR WITH TRANSITION ON OPACITY
+        modal.style.opacity = "0";
+
+        // WAIT FOR THE OPACITY TO REACH ZERO THEN TAKE MODAL OUT THE WINDOW
+        // YOU HAVE TO WAIT! OTHERWISE YOU ABRUPTLY SEE IT DISAPPEAR AND THE
+        // TRANSITION HAPPENS OUT OF WINDOW WHERE IT CAN'T BE SEEN
+        setTimeout(function(){
+            modal.style.transform = "translateY(-99.99999999%)";
+        }, 500);
+
+    }//if
+
+    // IF THE POP-UP IS INVISIBLE
+    else{
+
+        // SWITCH THE CONTROL VARIABLE ON
+        modal_ctrl = true;
+
+        // TAKE MODAL INSIDE THE WINDOW
+        modal.style.transform = "translateY(0%)";
+
+        // MAKE IT APPEAR WITH TRANSITION ON OPACITY
+        modal.style.opacity = "1";
+
+    }//else
+
+
+    /* 
+
+        SINCE THE EVENT IS ATTACHED TO A LINK YOU MUST PREVENT THE
+        AUTOMATIC BEHAVIOUR, WHICH IS THE CHANGE OF THE LOCATION
+
+    */ 
+    if(ev != null) ev.preventDefault();
+
+}//toggle_modal
+
 
