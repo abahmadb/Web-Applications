f13364d7ca4e08e0df5b55bae7dc73154fe9e5bd
xianwen jin <jinxw@live.it>
2020-05-02 20:15:15 +0200

    population

--- db_population.sql
+++ db_population.sql
@@ -3,12 +3,12 @@
 INSERT INTO 
     person(Name, Surname, Gender, DoB, Email, Passwd, Phone, Description) 
 VALUES 
-    ("Xianwen", "Jin", "M", "1995-02-28", "jinxw@live.it", SHA2("Jinxw95"), "3333333336", "Fuck the world"), 
-    ("Marco", "Grande", "M", "1996-02-10", "guess@live.it", SHA2("WantPass123"), "345678910", "don't fuck the world"),
-    ("Marco", "Piccolo", "M", "1995-11-28", "marco@live.it", SHA2("Guseee123"), "3333332222", "Love the world"), 
-    ("Victor", "Semencenco", "M", "1993-04-20", "sexy@live.it", SHA2("Gondus123"), "345678911", "Give a girl"),
-    ("Ahmad", "TooLong", NULL, "1995-01-30", "dontknow@live.it", SHA2("Lol16153"), "3333333336", NULL), 
-    ("Marco", "Grande", "F", NULL, "whoknows@live.it", SHA2("WantPass123"), NULL, NULL);
+    ("Xianwen", "Jin", "M", "1995-02-28", "jinxw@live.it", SHA2("Jinxw95",256), "3333333336", "Fuck the world"), 
+    ("Marco", "Grande", "M", "1996-02-10", "guess@live.it", SHA2("WantPass123",256), "345678910", "don't fuck the world"),
+    ("Marco", "Piccolo", "M", "1995-11-28", "marco@live.it", SHA2("Guseee123",256), "3333332222", "Love the world"), 
+    ("Victor", "Semencenco", "M", "1993-04-20", "sexy@live.it", SHA2("Gondus123",256), "345678911", "Give a girl"),
+    ("Ahmad", "TooLong", NULL, "1995-01-30", "dontknow@live.it", SHA2("Lol16153",256), "3333333336", NULL), 
+    ("Marco", "Grande", "F", NULL, "whoknows@live.it", SHA2("WantPass123",256), NULL, NULL);
 
 
 
