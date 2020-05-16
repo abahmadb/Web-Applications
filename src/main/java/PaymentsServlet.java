0194d947cc74c51e15746d3fdf0f21b0734266a6
AB <ahmad7bashir@gmail.com>
2020-05-16 18:02:40 +0200

    updates on PaymentsServlet

--- src/main/java/PaymentsServlet.java
+++ src/main/java/PaymentsServlet.java
@@ -3,12 +3,13 @@ import javax.servlet.*;
 import java.sql.*;
 import java.io.IOException;
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public final class PaymentsServlet extends DatabaseServlet {
 
 	//retrieve data from db
-	public void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		try {
 
@@ -16,7 +17,30 @@ public final class PaymentsServlet extends DatabaseServlet {
 			if (!IndexServlet.check_login(request))
 				throw new IOException("You need to be signed in to access this page");
 
-		}
+			HttpSession session = request.getSession();
+			Connection c = getConnection();
+			Statement st = c.createStatement();
+			DecimalFormat df = new DecimalFormat( "#.##" );
+			ResultSet rs = st.executeQuery("SELECT DATE_FORMAT(L.Payment, '%d/%m/%Y %H:%i') as payment_date, " +
+					"IF(L.TeacherID = " + session.getAttribute("userid") + ", CONCAT('Incoming deposit from ' , S.Name), CONCAT('Outgoing payment to ', T.Name)) as payment_desc, " +
+					"ROUND(TIME_TO_SEC(LessonDuration)*L.LessonTariff/3600, 2) as money_amount, " +
+					"IF(L.TeacherID = " + session.getAttribute("userid") + ", '+', '-') as payment_direction " +
+					"FROM (lesson as L JOIN person as T ON L.TeacherID=T.IDUser) JOIN person as S ON L.StudentID=S.IDUser " +
+					"WHERE L.Payment IS NOT NULL AND (TeacherID = " + session.getAttribute("userid") + " OR StudentID = " + session.getAttribute("userid") + ") " +
+					"ORDER BY L.Payment DESC LIMIT 4");
+
+
+			ArrayList<String> recent_payment = new ArrayList<String>();
+			while(rs.next())
+				recent_payment.add(new String(rs.getString("payment_date"),
+											rs.getString("payment_desc"),
+											rs.getString("payment_direction") + df.format(rs.getDouble("money_amount")) + " &euro;"));
+
+			request.setAttribute("recent_payment", String);
+			request.setAttribute("currentpage", request.getServletPath());
+			request.getRequestDispatcher("payments.jsp").forward(request, response);
+
+		}//try
 
 		//catch exception if we are not logged in
 		catch (IOException e) {
@@ -29,24 +53,9 @@ public final class PaymentsServlet extends DatabaseServlet {
 			} catch (Exception ignored) {
 			}
 
-		}
+		}//catch
+	}//doGet
 
-
-	/*
-
-	 */
-
-
-	}//try
-
-        /*catch(Exception ex)
-
-	{
-		req.setAttribute("error_message", ex.getMessage());
-		req.setAttribute("appname", req.getContextPath());
-		try {
-			req.getRequestDispatcher("errorpage.jsp").forward(req, res);
-		} catch (Exception e) {
-		}
-	}//catch*/
-}
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		}//doPost
+}//PaymentsServlet
