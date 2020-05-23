d92bab442c9ae8c1db7e9c811e523d136e9f1f6e
xianwen jin <jinxw@live.it>
2020-05-23 17:48:49 +0200

    minor changes and fix path

--- src/main/java/ProfileServlet.java
+++ src/main/java/ProfileServlet.java
@@ -1,7 +1,6 @@
 import javax.servlet.http.*;
 import javax.servlet.*;
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.io.IOException;
 import java.io.*;
 import java.nio.file.*;
@@ -28,12 +27,12 @@ public final class ProfileServlet extends DatabaseServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 		
+		// get the connection
 		Connection con = getConnection();
+		// initialize variables for queries
+		PreparedStatement st = null;
+		ResultSet rs = null;
 		
-		PreparedStatement st1 = null;
-		PreparedStatement st2 = null;
-		ResultSet rs1 = null;
-		ResultSet rs2 = null;
 		
 		try{
 			//check if we are logged in
@@ -48,8 +47,8 @@ public final class ProfileServlet extends DatabaseServlet{
 			req.setAttribute("person", person);
 			
 			// check if there exist the certificate and identity file for this userid
-			String identityFilePath = System.getProperty("user.dir") + "\\..\\webapps\\imageset\\identity\\" + idUser + ".jpg";
-            String certificateFilePath = System.getProperty("user.dir") + "\\..\\webapps\\imageset\\cerificate\\" + idUser + ".jpg";
+			String identityFilePath = System.getProperty("user.dir") + "./webapps/imageset/identity/" + idUser + ".jpg";
+            String certificateFilePath = System.getProperty("user.dir") + "./webapps/imageset/cerificate/" + idUser + ".jpg";
 			if (new File(identityFilePath).isFile())
 				req.setAttribute("identitytExists", true);
 			else 
@@ -61,38 +60,36 @@ public final class ProfileServlet extends DatabaseServlet{
 				req.setAttribute("certificateExists", false);
 			
 			
-			// topic info
+			// topic info for autocomplete
 			List<Topic> lista = new ArrayList<Topic>();
-            st1 = con.prepareStatement("SELECT * FROM topic");
-            rs1 = st1.executeQuery();
+            st = con.prepareStatement("SELECT * FROM topic");
+            rs = st.executeQuery();
 
-            while(rs1.next())
-                lista.add(new Topic(rs1.getInt("IDTopic"), rs1.getString("Label")));
-
+            while(rs.next())
+                lista.add(new Topic(rs.getInt("IDTopic"), rs.getString("Label")));
+			
+			// set the attribute which will be used in profile.jsp
             req.setAttribute("topics_list", lista);
 			
 			// topic form info
 			ArrayList<TeacherTopic> subjects = new ArrayList<TeacherTopic>();		
 			
-			final String STATEMENT_SELECT_TEACHER_TOPIC = "SELECT * FROM teacher_topic WHERE TeacherID=?";
-			final String STATEMENT_SELECT_TOPIC_NAME = "SELECT Label FROM Topic WHERE IDTopic=?";
-            st1 = con.prepareStatement(STATEMENT_SELECT_TEACHER_TOPIC);
-			st1.setInt(1, idUser);
-            rs1 = st1.executeQuery();
+			// queries
+			final String STATEMENT_SELECT_TEACHER_TOPIC = "SELECT TeacherID, TopicID, Tariff, Label FROM teacher_topic A INNER JOIN Topic T " +
+															"ON A.TopicID = T.IDTopic WHERE A.TeacherID=?";
 			
-			st2 = con.prepareStatement(STATEMENT_SELECT_TOPIC_NAME);
-            while(rs1.next()){				
-				st2.setInt(1, rs1.getInt("TopicID"));
-				rs2 = st2.executeQuery();
-				rs2.next();
-                subjects.add(new TeacherTopic(rs1.getInt("TeacherID"), rs1.getInt("TopicID"), rs1.getInt("Tariff"), rs2.getString("Label")));
+            st = con.prepareStatement(STATEMENT_SELECT_TEACHER_TOPIC);
+			st.setInt(1, idUser);
+            rs = st.executeQuery();
+			
+            while(rs.next()){				
+                subjects.add(new TeacherTopic(rs.getInt("TeacherID"), rs.getInt("TopicID"), rs.getInt("Tariff"), rs.getString("Label")));
 			}
-			req.setAttribute("first_subject", subjects.get(0));
-			//subjects.remove(0);
-            req.setAttribute("subject_list", subjects);			
 			
-			req.setAttribute("currentpage", req.getServletPath());
-			req.getRequestDispatcher("profile.jsp").forward(req, res);	
+			//req.setAttribute("first_subject", subjects.get(0));
+			//subjects.remove(0);
+			
+            req.setAttribute("subject_list", subjects);			
 			
 		} catch (SQLException ex){
 				
@@ -100,33 +97,26 @@ public final class ProfileServlet extends DatabaseServlet{
             req.setAttribute("appname", req.getContextPath());
             try{req.getRequestDispatcher("errorpage.jsp").forward(req, res);}	catch(Exception e){}
 			
+		// close statements and result sets	
 		} finally {
          		
-			if (st1 != null) {
+			if (st != null) {
                 try {
-                    st1.close();
+                    st.close();
                 } catch (SQLException ignored) { }
             }
 			
-			if (st2 != null) {
+			if (rs != null) {
                 try {
-                    st2.close();
+                    rs.close();
                 } catch (SQLException ignored) { }
             }
-			
-			if (rs1 != null) {
-                try {
-                    rs1.close();
-                } catch (SQLException ignored) { }
-            }
-			
-			if (rs2 != null) {
-                try {
-                    rs2.close();
-                } catch (SQLException ignored) { }
-            }
-
+	
         }	
+		
+		// set attributes and forward the request
+		req.setAttribute("currentpage", req.getServletPath());
+		req.getRequestDispatcher("profile.jsp").forward(req, res);	
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
@@ -145,17 +135,17 @@ public final class ProfileServlet extends DatabaseServlet{
 		else 
 			doUploadFile(req, res);
 		
+		// go back profile.jsp 
 		res.sendRedirect("profile");
-
-		
+	
 	}
 
+	// handle personal information form
 	public void doUpdatePerson(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException{
 				
-        //req.getSession().setAttribute("idUser", 1);
+        // get the user id
 		int idUser = Integer.parseInt(String.valueOf(req.getSession().getAttribute("userid")));	
-        //idUser = (int) req.getSession().getAttribute("idUser");
 	
 		try{
 			// retrieve the request parameters
@@ -170,8 +160,6 @@ public final class ProfileServlet extends DatabaseServlet{
 				gender = "";
 			}
 			String temp = req.getParameter("birth").toString();
-			/*SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-			dob = new java.sql.Date(format.parse(temp).getTime());*/
 			java.sql.Date dob = null;
 			try{
 				dob = java.sql.Date.valueOf(req.getParameter("birth").toString());
@@ -179,12 +167,12 @@ public final class ProfileServlet extends DatabaseServlet{
 			String email = req.getParameter("email");
 			String phone = req.getParameter("phone_nr");
 			String city = req.getParameter("city");
+			// password and description info are handled by other forms
 			String passwd = null, description = null;
 			// create person from the request parameters
 			Person person = new Person(idUser, name, surname, gender, dob, email, passwd, phone, city, description);
-			// update the information about person
+			// update the information about person using DAO
 			new UpdatePersonDAO(getConnection(), person).updatePerson();
-			
 			
 		} catch (SQLException ex){
 				
@@ -195,24 +183,31 @@ public final class ProfileServlet extends DatabaseServlet{
 		} 
 	}
 	
+	// manage password form
 	public void doUpdatePass(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException{
 		
+		// get user id
 		int idUser = Integer.parseInt(String.valueOf(req.getSession().getAttribute("userid")));		
+		
 		try{
-			
+			// get the passwords entered by user
 			String newPassword = req.getParameter("new_pw");
 			String oldPassword = req.getParameter("old_pw");
+			// find the person who matches user id and the password
 			Person person = new SearchPersonByIdDAO(getConnection(), idUser).searchPersonById();
+			// get the password of the user
 			String oldPassSHA = person.getPassword();
+			// update password using DAO
 			new UpdatePersonDAO(getConnection(), person).updatePassword(newPassword, oldPassword);
+			// get the new password (SHA256) of the user
 			Person personUpdated = new SearchPersonByIdDAO(getConnection(), idUser).searchPersonById();
 		
-			// check the password is changed
+			// check if the password is updated
 			if (!oldPassSHA.equals(personUpdated.getPassword())){
 				req.setAttribute("passMessage", "Updated Successfully");	
 			} else {
-				req.setAttribute("passMessage", "Update Failed.");	
+				req.setAttribute("passMessage", "Update Failed");	
 			}
 			
 		} catch (SQLException ex){
@@ -232,21 +227,27 @@ public final class ProfileServlet extends DatabaseServlet{
 													"SELECT ? " +
 													"FROM Dual " +
 													"WHERE NOT EXISTS (SELECT * FROM Topic WHERE Label=?)";*/
+		// Statement for queries
 		final String STATEMENT_DELETE_TEACHER_TOPIC = "DELETE FROM teacher_topic WHERE teacherID=?";
 		final String STATEMENT_INSERT_TEACHER_TOPIC = "INSERT INTO teacher_topic VALUES (?, (SELECT IDTopic FROM Topic WHERE Label=?), ?) " +
 													"ON DUPLICATE KEY UPDATE tariff=?";	
 													
 		int idUser = Integer.parseInt(String.valueOf(req.getSession().getAttribute("userid")));
+		// get the subjects and tariffs entered by user
 		String[] topics = req.getParameterValues("subject");
 		String[] tariffs = req.getParameterValues("tariff");
+		
+		// prepare for the query
 		Connection con = getConnection();
 		PreparedStatement st = null;
 		
 		try{
+			// by simplicity, just delete all the teacher_topics of the considered user
 			st = con.prepareStatement(STATEMENT_DELETE_TEACHER_TOPIC);
 			st.setInt(1, idUser);
 			st.execute();
 			
+			// insert teacher_topics
 			st = con.prepareStatement(STATEMENT_INSERT_TEACHER_TOPIC);
 			for (int i = 0; i < topics.length; i++){
 				st.setInt(1, idUser);
@@ -257,8 +258,6 @@ public final class ProfileServlet extends DatabaseServlet{
 			
 				st.execute();	
 			}
-			
-			
 
 			req.setAttribute("topicMessage", "Updated");
 			
@@ -280,6 +279,7 @@ public final class ProfileServlet extends DatabaseServlet{
         }			
 	}
 	
+	// handle description form
 	public void doUpdateDescription(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException{
 				
@@ -291,6 +291,7 @@ public final class ProfileServlet extends DatabaseServlet{
 		try{
 			
 			int idUser = Integer.parseInt(String.valueOf(req.getSession().getAttribute("userid")));
+			// get the description inserted by user (it's a string of html code)
 			String text = req.getParameter("text");
 			// update the description
 			st = con.prepareStatement(STATEMENT);
@@ -321,6 +322,10 @@ public final class ProfileServlet extends DatabaseServlet{
         }
 	}
 	
+	/*  handle the three upload file forms
+		in particular they will be renamed according to user id and uploaded into a specific directory
+		for simplicity we just handle jpg file
+	*/
 	public void doUploadFile(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException{
 				
@@ -346,8 +351,6 @@ public final class ProfileServlet extends DatabaseServlet{
         upload.setFileSizeMax(MAX_FILE_SIZE);
         upload.setSizeMax(MAX_REQUEST_SIZE);
 
-        // relative path
-        //String uploadPath = request.getServletContext().getRealPath("./") + File.separator + UPLOAD_DIRECTORY;
 		String upload_dir = null;
 		String uploadPath = null;
         String filePath = null;
@@ -376,7 +379,7 @@ public final class ProfileServlet extends DatabaseServlet{
 							upload_dir = "certificate";
 						}
 						// set the upload path 
-						uploadPath = System.getProperty("user.dir") + "\\..\\webapps\\imageset\\" + upload_dir;
+						uploadPath = System.getProperty("user.dir") + "./webapps/imageset/" + upload_dir;
                         filePath = uploadPath + File.separator + fileName;
                         File storeFile = new File(filePath);
                   
@@ -387,6 +390,7 @@ public final class ProfileServlet extends DatabaseServlet{
                     }
                 }
             }
+			
         } catch (Exception ex) {
             req.setAttribute("fileMessage", "upload failed");
 			req.setAttribute("error_message", ex.getMessage());
