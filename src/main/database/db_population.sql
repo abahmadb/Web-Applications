a7aa1a8f5a60046acf83530121464086fa087b1d
Marco Dell'Anna <marco.dellanna@studenti.unipd.it>
2020-06-03 14:47:55 +0200

    db_population changes

--- src/main/database/db_population.sql
+++ src/main/database/db_population.sql
@@ -32,7 +32,7 @@ INSERT INTO topic VALUES (NULL, "JSP"), (NULL, "Ruby"), (NULL, "Perl"), (NULL, "
 
 -- TEACHER_TOPIC
 -- ALL OF US AS MYSQL TEACHERS
-INSERT INTO teacher_topic VALUES (1, 29, 13),(2, 29, 14),(3, 29, 12),(4, 29, 10),(5, 29, 16),(6, 29, 12), (4, 25, 10), (4, 24, 10), (4, 21, 10), (6, 30, 20), (3, 8, 30);
+INSERT INTO teacher_topic VALUES (1, 29, 13),(2, 29, 14),(2, 30, 15),(3, 29, 12),(4, 29, 10),(5, 29, 16),(6, 29, 12), (4, 25, 10), (4, 24, 10), (4, 21, 10), (6, 30, 20), (3, 8, 30);
 
 
 
@@ -44,17 +44,16 @@ VALUES
     (2, 3, '2020-05-30', '12:00', '1:00', 18, '2020-05-28 23:59:59'),
     (2, 4, '2020-05-15', '08:30', '2:00', 14, '2020-05-13 23:59:59'),
     (1, 5, '2020-06-02', '11:00', '0:30', 15, '2020-05-30 23:59:59'),
-    (2, 1, '2020-06-12', '08:30', '2:00', 13, '2020-06-10 23:59:59'),
-    (3, 6, '2020-06-20', '14:00', '2:30', 16, '2020-06-18 23:59:59');
+    (2, 1, '2020-06-12', '08:30', '2:00', 13, '2020-04-10 23:59:59'),
+    (3, 6, '2020-06-20', '14:00', '2:30', 16, '2020-04-18 23:59:59');
 	
 	
 INSERT INTO
 	feedback
 VALUES
 	(2, 3, 4, "Very good", '2020-05-13'),
-	(2, 1, 4, "Excellent", '2020-05-13'),
+	(2, 6, 5, "Impressive teacher! Very much recommended" , '2020-03-13'),
 	(2, 4, 5, "If you need a prepared, reliable, communicative and helpful person, contact him, you won't be disappointed", '2020-05-13'),
-	(1, 3, 4, "Precise and helpful, recommended :)", '2020-05-13'),
 	(4, 1, 4, "Patient and prepared", '2020-05-13'),
     (4, 2, 1, "Patient and prepared2", '2020-05-13'),	
 	(4, 3, 3, "Patient and prepared3", '2020-05-13'),	
